aba0c14b9ca89dca4131db470ad68c480be4025d
Hải Lê <HadesD@users.noreply.github.com>
2017-07-12 11:42:42 +0900

    Jquery add

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,22 @@
+var COMPLIER = 'https://paiza.io/api/projects.json';
+$('#complie').click(function(){
+  $(this).attr('disabled', 'disable');
+  var COMPLIE_BTN = $(this);
+  var data = {
+    
+  };
+  $.ajax({
+    url: COMPLIER,
+    headers: {
+      'X-XSRF-TOKEN':'"CYMwJDo2lYPQJ6vXHNjzaDSOeeWfXBBWDUpt/yKqDyZEh0WFwUgzeEYYY02hMIITm5pZxAZLFyyScpjg9A0RyA=="'
+    },
+    method: 'POST',
+    data: '{"project":{"id":506521,"source_files":[{"filename":"Main.cpp","body":"#include <iostream>\nusing namespace std;\nint main(void){\n    // Here your code !\n    \n}\n","position":0},{"id":634715,"_destroy":true}],"language":"cpp","input":null,"share":"public","network":true,"live":null,"output_type":null,"schedule":null,"title":null},"run":true,"save":true}',
+    success: function(data) {
 
+    },
+    complete: function(data) {
+      COMPLIE_BTN.removeAttr('disabled');
+    }
+  });
+});
