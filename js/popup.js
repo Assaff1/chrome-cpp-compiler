9449f2841f8e5087e6ede1767dbb372f54e06b61
Hai Le <levanhai@u-shop.co.jp>
2017-07-12 16:02:43 +0900

    Permission for request POST

--- js/popup.js
+++ js/popup.js
@@ -3,15 +3,24 @@ $('#complie').click(function(){
   $(this).attr('disabled', 'disable');
   var COMPLIE_BTN = $(this);
   var data = {
-    
+    "project": {
+      "language": "cpp",
+      "source_files": [
+	{
+	  "filename": "Main.cpp",
+	  "body": editor.getValue()
+	}
+      ],
+    }
   };
   $.ajax({
     url: COMPLIER,
     headers: {
-      'X-XSRF-TOKEN':'"CYMwJDo2lYPQJ6vXHNjzaDSOeeWfXBBWDUpt/yKqDyZEh0WFwUgzeEYYY02hMIITm5pZxAZLFyyScpjg9A0RyA=="'
+      'Content-Type':'application/json',
+      'X-XSRF-TOKEN':'"q3ebbG6+VN/N34CdbaGjTLL/vGqftE/SJSIh1idubi43FQdoQR3gJ5MMAmH9a+FqmGFEtwPu8aW0otS6VDg0vw=="'
     },
     method: 'POST',
-    data: '{"project":{"id":506521,"source_files":[{"filename":"Main.cpp","body":"#include <iostream>\nusing namespace std;\nint main(void){\n    // Here your code !\n    \n}\n","position":0},{"id":634715,"_destroy":true}],"language":"cpp","input":null,"share":"public","network":true,"live":null,"output_type":null,"schedule":null,"title":null},"run":true,"save":true}',
+    data: JSON.stringify(data),
     success: function(data) {
 
     },
