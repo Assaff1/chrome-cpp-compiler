6740627f2aa329bda2d27a751eb49450c2ae5f78
Dark.Hades <dark.hades.1102@gmail.com>
2017-07-16 07:52:12 +0900

    Gulp added

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+var bases = {
+  app: 'app/',
+  dist: 'dist/',
+};
+
+var paths = {
+  js: ['js/*.js'],
+  css: ['css/*.css'],
+  jslibs: ['js/**/*.js', '!js/*.js']
+}
+
+gulp.task('css', function(){
+  gulp.src(paths.css, {cwd: bases.app})
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest(bases.dist + 'css'));
+});
+
+gulp.task('js', function(){
+  gulp.src(paths.js, {cwd: bases.app})
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(bases.dist + 'js'));
+});
+
+gulp.task('copy', function(){
+  // Copy HTML
+  gulp.src('*.html', {cwd: bases.app})
+    .pipe(gulp.dest(bases.dist));
+
+  // Copy images
+  gulp.src('*.png', {cwd: bases.app})
+    .pipe(gulp.dest(bases.dist));
+
+  // Copy json
+  gulp.src('*.json', {cwd: bases.app})
+    .pipe(gulp.dest(bases.dist));
+
+  // Copy JS Libraries
+  gulp.src(paths.jslibs, {cwd: bases.app + '**'})
+    .pipe(gulp.dest(bases.dist));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(bases.app + '**/*', ['css', 'js', 'copy']);
+  //gulp.watch([''], ['']);
+});
+
+gulp.task('build', ['css', 'js', 'copy', 'watch']);
